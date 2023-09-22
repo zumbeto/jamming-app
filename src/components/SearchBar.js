dfd35e0877bc5624f16044d190853fba1f0f5306
Stoyan Peev <stoyanilievpeev@gmail.com>
2023-09-23 00:18:40 +0100

    feat: Implement multiple components and Spotify authentication
    - Implemented Spotify authentication logic using environment variables.
    - Created SearchResults component to display track details and album cover.
    - Initialized Playlist component with static mockup data for now.
    - Implemented MobileNav component for toggling between search results and playlist
    views.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SearchBar.module.css';
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setQuery(value);
+    setIsValid(value.length > 0);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isValid && onSearch) {
+      onSearch(query);
+    }
+  };
+
   return (
     <div className={styles['container']}>
-      <input
-        className={styles['search-bar']}
-        type='text'
-        placeholder='Search...'
-      />
-      <button className={styles['search-bar__btn']}>Search</button>
+      <form
+        className={styles.search}
+        onSubmit={handleSubmit}
+      >
+        <input
+          className={styles['search-bar']}
+          type='text'
+          placeholder='Search...'
+          value={query}
+          onChange={handleChange}
+        />
+        <button
+          className={styles['search-bar__btn']}
+          disabled={!isValid}
+          type='submit'
+        >
+          Search
+        </button>
+      </form>
     </div>
   );
 };
